29a42eb48f59b89a182d4dc4b601f3017bf55e30
Simone Gigante <92672883+simoncraf@users.noreply.github.com>
2024-05-14 09:16:00 +0200

    feat: Distance matrix

--- node.py
+++ node.py
@@ -12,5 +12,5 @@ class Node:
     def __repr__(self):
         return f"Node(index={self.index}, x={self.x}, y={self.y}, score={self.score})"
 
-    def distance_to(self, other_node):
+    def distance_to(self, other_node) -> float:
         return round(math.sqrt((self.x - other_node.x) ** 2 + (self.y - other_node.y) ** 2),4)
--- savings_heuristic.py
+++ savings_heuristic.py
@@ -1,4 +1,9 @@
+from node import Node
 from route import Route
+
+def savings_heuristic(nodes, origin, destination, battery_limit):
+    dummy_solution = generate_dummy_soultion(nodes, origin, destination, battery_limit)
+    distance_matrix = _compute_distance_matrix(nodes)
 
 def generate_dummy_soultion(nodes, origin, destination, battery_limit):
     routes = []
@@ -9,3 +14,16 @@ def generate_dummy_soultion(nodes, origin, destination, battery_limit):
             route.add_node(destination)
             routes.append(route)
     return routes
+
+def _compute_distance_matrix(nodes: list[Node]):
+    num_nodes = len(nodes)
+    distance_matrix = [[0] * num_nodes for _ in range(num_nodes)]
+    
+    for i in range(num_nodes):
+        for j in range(i + 1, num_nodes):
+            if i != j:
+                distance = nodes[i].distance_to(nodes[j])
+                distance_matrix[i][j] = distance
+                distance_matrix[j][i] = distance
+
+    return distance_matrix
